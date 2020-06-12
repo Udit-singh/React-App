21c07e8fe6338ba2fb7e13e31ee3956b7506148e
Udit singh <Udit-singh@users.noreply.github.com>
2020-06-12 16:35:29 +0530

    Footer Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponents';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -24,6 +25,7 @@ class Main extends Component {
         <Header />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter(dish => { return dish.id === this.state.selectedDish})[0]} />
+        <Footer />
       </div>
     );
   }
