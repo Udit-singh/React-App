d92721713ff5b8904c8899659b48f069ce11fc5f
Udit singh <Udit-singh@users.noreply.github.com>
2020-06-13 00:06:48 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   
   render() {
     return (
-      <div>
+      <BrowserRouter>
+       <div>
         <Main />
       </div>
+      </BrowserRouter>
+     
     );
   }
 }
