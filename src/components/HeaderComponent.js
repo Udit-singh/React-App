f8847ca0d7260c36b691287a824bad2e85ed407c
Udit singh <Udit-singh@users.noreply.github.com>
2020-06-16 14:53:58 +0530

    Login event fix

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -12,7 +12,7 @@ class Header extends Component {
     };
     this.toggleNav = this.toggleNav.bind(this);  
     this.toggleModal = this.toggleModal.bind(this);
-    
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   toggleNav() {
@@ -27,6 +27,12 @@ class Header extends Component {
     });
   }
 
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value
+        + " Remember: " + this.remember.checked);
+    event.preventDefault();
+}
 
   render() {
     return(
